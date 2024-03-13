16c30d7d9d638a593add406e1ee6c4e83a401544
Shcare <shcareee@gmail.com>
2024-03-13 07:59:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+
+// function superbowlWin(record) {
+//   if (record.find((i) => i.result === "W")) {
+//     return record.find((i) => i.result === "W").year;
+//   } else {
+//     return record.find((i) => i.result === "W");
+//   }
+// }
+
+function superbowlWin(record) {
+  let o = record.find((i) => i.result === "W");
+  if (o) {
+    return o.year;
+  } else {
+    return o;
+  }
+}
+//   if (object.result === "W") {
+//     return object.year;
+//   } else {
+//     return object.find(0);
+//   }
+// }
+// let o = record.find((i) => i.result === "W");
+// if (o.result === "W") {
+//   return o.year;
+// } else {
+//   return undefined;
